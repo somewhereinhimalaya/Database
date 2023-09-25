c2983f5ee47eed27df9bc8da11094d0be66d95a6
whereyoucan'tfind <74447376+batmanisvectorsigma@users.noreply.github.com>
2023-09-25 20:09:57 +0545

    Update firstcreatetable.sql

--- tutorial from website/Create Tables/firstcreatetable.sql
+++ tutorial from website/Create Tables/firstcreatetable.sql
@@ -1,1 +1,17 @@
+---------------------------------------------------CREATE TABLE--------------------------------------------------------------------
+--Example with no primary key
+create table customers(
+  customer_id number(10) not null,
+  customer_name varchar2(50) not null,
+  city varchar2(50)
+  );
+
+--Example with primary key
+create table customers(
+  customer_id number(10) not null,
+  1customer_name varchar2(50) not null,
+  city varchar2(50),
+  constraint customers_pk primary key(customer_id)
+  );
+
 

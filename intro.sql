364020601a0da8966cf15da93420120b913ad3b1
whereyoucan'tfind <74447376+batmanisvectorsigma@users.noreply.github.com>
2023-09-27 00:45:21 -0700

    Update intro.sql

--- intro.sql
+++ intro.sql
@@ -11,6 +11,10 @@ create user anishbro identified by 8080;
 grant connect,resource to anishbro
   --grant create table access to user
   grant create table to anishbro
+  --provide resource or space in the  database so users can create tables
+  -- here you don't need to make datafile.dbf it is itself created
+  CREATE TABLESPACE custom_tablespace 
+DATAFILE '/path/to/datafile.dbf' SIZE 100M;
 --THIRD STEP(OPTIONAL)
 
       --LIST ALL THE TABLE USING THIS QUERY

fbd282a99eab38a2421e1b79b565a15eb622482a
whereyoucan'tfind <74447376+batmanisvectorsigma@users.noreply.github.com>
2023-09-27 00:47:11 -0700

    Update intro.sql

--- intro.sql
+++ intro.sql
@@ -9,12 +9,17 @@
     --here anishbro is username and 8080 is a password
 create user anishbro identified by 8080;
 grant connect,resource to anishbro
+  
   --grant create table access to user
   grant create table to anishbro
+  
   --provide resource or space in the  database so users can create tables
   -- here you don't need to make datafile.dbf it is itself created
   CREATE TABLESPACE custom_tablespace 
 DATAFILE '/path/to/datafile.dbf' SIZE 100M;
+
+--connect to user 
+connect user 
 --THIRD STEP(OPTIONAL)
 
       --LIST ALL THE TABLE USING THIS QUERY

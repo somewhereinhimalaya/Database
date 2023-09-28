c9bd7370ff6e207f1cdf5251b802be1412a6f7c0
whereyoucan'tfind <74447376+batmancanbevectorsigma01@users.noreply.github.com>
2023-09-28 20:19:25 +0545

    Update oraclequery.sql

--- tutorial from website/Create Tables/oraclequery.sql
+++ tutorial from website/Create Tables/oraclequery.sql
@@ -51,3 +51,75 @@ order by name;
 
 
 -----------------------------------------------ORACLE INSERT STATEMENT--------------------------------------------------------------------
+
+--Example 1
+          --Insert by value keyword
+insert into suppliers(supplier_id,supplier_name) values(50,'Flipkart');
+
+--Example 2
+          --Insert by select statement
+insert into suppliers(supplier_id,supplier_name)
+select age,address
+from customers
+where age>20;
+
+--Example 3
+        --Check the number of rows that you want to insert
+select count(*)
+from customers
+where age>20;
+
+
+
+------------------------------------------------ORACLE INSERTALL STATEMENT----------------------------------------------------------------
+
+--Example 1
+        --Insert multiple records in one table 
+           --insert 3 rows into the suppliers table
+insert all
+  into suppliers(supplier_id,supplier_name) values(20,'Google')
+  into suppliers(supplier_id,supplier_name) values(21,'Microsoft')
+  into suppliers(supplier_id,supplier_name) values(22,'Apple')
+select * from dual
+
+--Example 2 
+        --Insert multiple records in a single tavble 
+          --Using single line command for each statement
+insert into suppliers(supplier_id,supplier_name) values(1000,'Google')
+insert into suppliers(supplier_id,supplier_name) values(2000,'Microsoft')
+insert into suppliers(supplier_id,supplier_name) values(3000,'Apple')
+
+--Example 3
+          --Insert into multiple table
+insert all
+    into suppliers(supplier_id,supplier_name) values(30,'Google')
+    into suppliers(supplier_id,supplier_name) values(31,'Microsoft')
+    into customers(age,name,address) values(29,'Luca Warsi','New York')
+select * from dual;
+
+
+
+------------------------------------------ORACLE UPDATE STATEMENT----------------------------------------------------------------
+
+--Example 1 
+        --Oracle update Example(Update Single Column)
+update suppliers
+set supplier_name='kingfisher'
+where supplier_id=29;
+
+--Example 2
+        --Update multiple columns of a table with a single statement
+update suppliers
+set supplier_address='Agra'
+    supplier_name='Bata Shoes'
+where supplier_id=1;
+
+--Example 3
+        --Oracle update Example: By selecting records from another table
+update customers
+set name=(select supplier_name from suppliers
+          where suppliers.supplier_name=customers.name)
+where age<25;
+
+
+-------------------------------------------ORACLE DELETE STATEMENT-------------------------------------------------------------------

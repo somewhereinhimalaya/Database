bf595fca5d2ed008ae64fc593172fd47609ccf97
whereyoucan'tfind <74447376+batmancanbevectorsigma01@users.noreply.github.com>
2023-09-28 19:08:15 +0545

    Update oraclequery.sql

--- tutorial from website/Create Tables/oraclequery.sql
+++ tutorial from website/Create Tables/oraclequery.sql
@@ -5,3 +5,9 @@ select * from customers
 insert into customers values(101,'rahul','delhi');
 --Oracle Update Query
 update customers set name='bob', city='london' where id=101;
+--Oracle Delete Query
+delete from customers where idc=101;
+--Oracle Truncate Query
+truncate table customers;
+--Oracle Drop Query
+drop table customer;

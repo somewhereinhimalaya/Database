04b04861bfb1f4e68a8bb74818720be214f18a27
whereyoucan'tfind <74447376+batmancanbevectorsigma01@users.noreply.github.com>
2023-09-28 19:01:40 +0545

    Update oraclequery.sql

--- tutorial from website/Create Tables/oraclequery.sql
+++ tutorial from website/Create Tables/oraclequery.sql
@@ -1,1 +1,7 @@
+--------------------------------------------A QUICK OVERVIEW OF ORACLE------------------------------------------------------------------
 --Oracle Select Query
+select * from customers
+--Oracle Insert Query
+insert into customers values(101,'rahul','delhi');
+--Oracle Update Query
+update customers set name='bob', city='london' where id=101;

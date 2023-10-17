3283560cda1af8f5ec67abc9afeb078ad91f34bb
whereyoucan'tfind <74447376+batmainisvectorsigma@users.noreply.github.com>
2023-10-17 13:36:17 +0545

    Update views.sql

--- tutorial from website/Oracle views/views.sql
+++ tutorial from website/Oracle views/views.sql
@@ -17,3 +17,17 @@ INSERT ALL
     INTO order1(order_number, supplier_id, city) VALUES(101, 1, 'Allahabad')
     INTO order1(order_number, supplier_id, city) VALUES(102, 2, 'Kanpur')
 SELECT * FROM dual;
+
+
+CREATE VIEW sup_orders AS  
+SELECT suppliers.supplier_id, order1.quantity, order1.price  
+FROM suppliers  
+INNER JOIN order1  
+ON suppliers.supplier_id = supplier_id  
+WHERE suppliers.supplier_name = 'VOJO';
+
+SELECT * FROM sup_orders;
+--facing error but why
+
+
+

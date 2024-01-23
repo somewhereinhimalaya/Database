40d47322f9bfaff77c237d06bafc669b6c4af6e3
whereyoucanfindme <74447376+whocanbevectorsigma@users.noreply.github.com>
2024-01-23 06:19:18 +0545

    Update rough_save.sql

--- rough_save.sql
+++ rough_save.sql
@@ -41,3 +41,41 @@ alter table customers rename to hustomers;
 alter table hustomers rename to customers;
 
 --INSERTING VALUE IN CUSTOMERS TABLE
+insert all
+into customers(customer_id,customer_name,city,customer_dob) values(1,'Hitesh Sharma','Delhi',to_date('1990-12-12','yyyy-mm-dd'))
+into customers(customer_id,customer_name,city,customer_dob) values(2,'Sitesh Sharma','Delhi',to_date('1991-12-12','yyyy-mm-dd'))
+select * from dual;
+delete from customers where customer_id = 1;
+delete from customers where customer_id = 2;
+
+--NOW INSERTING VALUE FOR REAL
+insert all
+into customers(customer_id,customer_name,city,customer_dob) values(1,'Hitesh Sharma','Delhi',to_date('1990-12-12','yyyy-mm-dd'))
+into customers(customer_id,customer_name,city,customer_dob) values(2,'Sitesh Sharma','Kathmandu',to_date('1991-12-12','yyyy-mm-dd'))
+select * from dual;
+*/
+--desc employee;
+
+insert all 
+into employee(employee_id,customer_id,employee_name,employee_trainer,employee_dob) values(1,1,'Hitesh Sharma','Normi',to_date('1990-12-12','yyyy-mm-dd'))
+into employee(employee_id,customer_id,employee_name,employee_trainer,employee_dob) values(2,2,'Sitesh Sharma','Sormi',to_date('1991-12-12','yyyy-mm-dd'))
+select * from dual;
+
+-- Using orderby
+select *
+from employee 
+where employee_id>0
+order by customer_id desc;
+
+select * 
+from customers
+where customer_id > 0
+order by customer_id asc
+
+-- joining table
+select customers.customer_name,employee.employee_name,employee.employee_dob
+from customers --primary key
+inner join employee  --foreign key
+on customers.customer_id=employee.customer_id;
+
+

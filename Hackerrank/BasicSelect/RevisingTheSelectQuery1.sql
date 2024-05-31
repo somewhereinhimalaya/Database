54c0b0dcf9763d783f78b2f6795fb9452e86e1e4
whereyoucanfindme <74447376+vectorsigmaissomewhere@users.noreply.github.com>
2024-05-31 22:53:05 +0545

    Create RevisingTheSelectQuery1.sql

---
+++ Hackerrank/BasicSelect/RevisingTheSelectQuery1.sql
@@ -0,0 +1,9 @@
+SELECT * FROM CITY WHERE POPULATION > 100000 AND COUNTRYCODE = 'USA';
+
+/*
+3878 Scottsdale USA Arizona 202705 
+3965 Corona USA California 124966 
+3973 Concord USA California 121780 
+3977 Cedar Rapids USA Iowa 120758 
+3982 Coral Springs USA Florida 117549 
+*/
